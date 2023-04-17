0db994a92b0f43ed54b91d228484f4140567b6de
Greg Martinjak <greg.martinjak@gmail.com>
2023-04-17 17:05:12 -0400

    Updated second window with chore names

--- UI.py
+++ UI.py
@@ -4,56 +4,77 @@ from PIL import Image, ImageTk
 # create the main window
 root = tk.Tk()
 
-def printcmd(input):
+def chore_select(input):
     print(input.get())
     root.withdraw()
     new_window = tk.Toplevel()
     new_window.title("Chore Selection")
-    new_window.geometry("600x100+800+400")
-    label = tk.Label(new_window, text="This is a new window!")
-    label.pack()
+    new_window.geometry("480x130+800+400")
 
+    label=tk.Label(new_window)
+    label.grid(row=0,column=0)
+
+    chore1 = tk.Button(new_window, text="Clean the cat litter", height=2)
+    chore1.grid(row=1, column=0)
+    chore2 = tk.Button(new_window, text="Put away clean dishes", height=2)
+    chore2.grid(row=1, column=1)
+    chore3 = tk.Button(new_window, text="Put away dirty dishes", height=2)
+    chore3.grid(row=1, column=2)
+    chore4 = tk.Button(new_window, text="Take out the trash", height=2)
+    chore4.grid(row=1, column=3)
+
+    label=tk.Label(new_window)
+    label.grid(row=2,column=0)
+
+    chore5 = tk.Button(new_window, text="Put away laundry",  height=2)
+    chore5.grid(row=3, column=0)
+    chore6 = tk.Button(new_window, text="Mow the lawn",  height=2)
+    chore6.grid(row=3, column=1)
+    chore7 = tk.Button(new_window, text="Vacuum the carpet",  height=2)
+    chore7.grid(row=3, column=2)
+    chore8 = tk.Button(new_window, text="Tidy your room",  height=2)
+    chore8.grid(row=3, column=3)
 
 # create an image object
 image = Image.open("Images\Me\me.jpg")
 image = image.resize((300, 300), Image.LANCZOS)
-photo1 = ImageTk.PhotoImage(image)
+greg_photo = ImageTk.PhotoImage(image)
 
 image = Image.open("Images\Alicia\IMG_5956.JPG")
 image = image.resize((300, 300), Image.LANCZOS)
-photo2 = ImageTk.PhotoImage(image)
+alicia_photo = ImageTk.PhotoImage(image)
 
 image = Image.open("Images\Aria\IMG_5992.PNG")
 image = image.resize((300, 300), Image.LANCZOS)
-photo3 = ImageTk.PhotoImage(image)
+aria_photo = ImageTk.PhotoImage(image)
 
 image = Image.open("Images\Mae\IMG_5983.JPG")
 image = image.resize((300, 300), Image.LANCZOS)
-photo4 = ImageTk.PhotoImage(image)
+mae_photo = ImageTk.PhotoImage(image)
 
 root.title("Who is reporting the chore?")
 root.geometry("600x600+800+300")
 
 # create a 2x2 grid of buttons
-entry_string1 = tk.StringVar(value = "Greg")
-button1 = tk.Button(root, image=photo1, command=lambda:printcmd(entry_string1))
-button1.image = photo1  # keep a reference to the photo to prevent garbage collection
-button1.grid(row=0, column=0)
+greg_selection = tk.StringVar(value = "Greg")
+greg_button = tk.Button(root, image=greg_photo, command=lambda:chore_select(greg_selection))
+greg_button.image = greg_photo  # keep a reference to the photo to prevent garbage collection
+greg_button.grid(row=0, column=0)
 
-entry_string2 = tk.StringVar(value = "Alicia")
-button2 = tk.Button(root, image=photo2, command=lambda:printcmd(entry_string2))
-button2.image = photo2
-button2.grid(row=0, column=1)
+alicia_selection = tk.StringVar(value = "Alicia")
+alicia_button = tk.Button(root, image=alicia_photo, command=lambda:chore_select(alicia_selection))
+alicia_button.image = alicia_photo
+alicia_button.grid(row=0, column=1)
 
-entry_string3 = tk.StringVar(value = "Aria")
-button3 = tk.Button(root, image=photo3, command=lambda:printcmd(entry_string3))
-button3.image = photo3
-button3.grid(row=1, column=0)
+aria_selection = tk.StringVar(value = "Aria")
+aria_button = tk.Button(root, image=aria_photo, command=lambda:chore_select(aria_selection))
+aria_button.image = aria_photo
+aria_button.grid(row=1, column=0)
 
-entry_string4 = tk.StringVar(value = "Mae")
-button4 = tk.Button(root, image=photo4, command=lambda:printcmd(entry_string4))
-button4.image = photo4
-button4.grid(row=1, column=1)
+mae_selection = tk.StringVar(value = "Mae")
+mae_button = tk.Button(root, image=mae_photo, command=lambda:chore_select(mae_selection))
+mae_button.image = mae_photo
+mae_button.grid(row=1, column=1)
 
 # run the main loop
 root.mainloop()
